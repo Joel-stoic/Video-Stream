8be7c521ed1b54d1d2eb04aed81e158abea9d4d8
Joel <joelimmanual.off@gmail.com>
2026-03-07 01:32:28 +0530

    feat: enhance video upload functionality with multer configuration and integrate middleware in upload route

--- backend/routes/upload.route.js
+++ backend/routes/upload.route.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import { uploadController } from '../controllers/video.controller'
 import { protect } from '../middleware/protect.js'
+import { uploadMiddleware } from '../config/multer.js'
 
 
 const router=express.Router()
 
 
-router.post('upload',protect,uploadController)
+router.post('upload',protect,uploadMiddleware.single('video'),uploadController)
 
 export default router
